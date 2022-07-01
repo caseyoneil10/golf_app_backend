eca6bf68dcff4b310d7c3f542879b21dd090fe5d
Casey O'Neil <caseyoneil@haleys-MacBook-Pro.local>
2022-07-01 13:02:34 -0700

    model adjustment

--- golfapp/golfapp/urls.py
+++ golfapp/golfapp/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls import include
 urlpatterns = [
     path('', include('golf_score_api.urls')),
     path('', include('course_data_api.urls')),
+    path('', include('holeScore_api.urls')),
     path('admin/', admin.site.urls),
 ]

58d34082ba28ea19dd5ddf7e7e737283bac96275
Casey O'Neil <caseyoneil@haleys-MacBook-Pro.local>
2022-07-01 12:27:23 -0700

    updated seriailizer

--- golfapp/holeScore_api/models.py
+++ golfapp/holeScore_api/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
 
-
 class HoleScore(models.Model):
-    score = models.IntegerField()
+    hole1Score = models.IntegerField()
+    hole2Score = models.IntegerField()
+    hole3Score = models.IntegerField()
+    hole4Score = models.IntegerField()
+    hole5Score = models.IntegerField()
+    hole6Score = models.IntegerField()
+    hole7Score = models.IntegerField()
+    hole8Score = models.IntegerField()
+    hole9Score = models.IntegerField()
+    hole10Score = models.IntegerField()
+    hole11Score = models.IntegerField()
+    hole12Score = models.IntegerField()
+    hole13Score = models.IntegerField()
+    hole14Score = models.IntegerField()
+    hole15Score = models.IntegerField()
+    hole16Score = models.IntegerField()
+    hole17Score = models.IntegerField()
+    hole18Score = models.IntegerField()
--- golfapp/holeScore_api/serializers.py
+++ golfapp/holeScore_api/serializers.py
@@ -4,4 +4,23 @@ from .models import HoleScore
 class HoleScoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = HoleScore
-        fields = ('id', 'score',)
+        fields = ('id',
+        'hole1Score',
+        'hole2Score',
+        'hole3Score',
+        'hole4Score',
+        'hole5Score',
+        'hole6Score',
+        'hole7Score',
+        'hole8Score',
+        'hole9Score',
+        'hole10Score',
+        'hole11Score',
+        'hole12Score',
+        'hole13Score',
+        'hole14Score',
+        'hole15Score',
+        'hole16Score',
+        'hole17Score',
+        'hole18Score',
+        )
